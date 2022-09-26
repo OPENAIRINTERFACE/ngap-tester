1fea106a2b2c9d1156c9fad60a4fc341e1aced7d
Raphael Defosseux <raphael.defosseux@openairinterface.org>
2022-09-26 16:17:25 +0200

    chore(ci): adding golang linter
    Signed-off-by: Raphael Defosseux <raphael.defosseux@openairinterface.org>

--- scripts/sync-for-pull-request.sh
+++ scripts/sync-for-pull-request.sh
@@ -2,12 +2,12 @@
 # for each sub-module
 # If you want to re-trigger the CI jobs with the same branches
 # But new commits, just add the commit SHA-ONE as comment
-# gnbsim branch: 5fbe198c2249a18566bd7bdd0a5e0fa5ee944fb7
+# gnbsim branch: N/A
 # nas branch: N/A
 # ngap branch: N/A
 ./scripts/sync-sub-modules.py \
    --synchronize \
    --force \
-   --gnbsim dockerfile-optimization \
+   --gnbsim ngap-tester \
    --nas ngap-tester \
    --ngap ngap-tester
