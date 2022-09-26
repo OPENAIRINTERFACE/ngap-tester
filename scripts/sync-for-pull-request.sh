d58cf4d6834a1bbeb1f339d2056a7e1e2731be03
Raphael Defosseux <raphael.defosseux@openairinterface.org>
2022-09-26 18:27:36 +0200

    fix(ci): updating golangci-lint version and fixed branches for nas/ngap
    Signed-off-by: Raphael Defosseux <raphael.defosseux@openairinterface.org>

--- scripts/sync-for-pull-request.sh
+++ scripts/sync-for-pull-request.sh
@@ -3,11 +3,11 @@
 # If you want to re-trigger the CI jobs with the same branches
 # But new commits, just add the commit SHA-ONE as comment
 # gnbsim branch: N/A
-# nas branch: N/A
-# ngap branch: N/A
+# nas branch: test-go-linter # 7cfeb68a8a2d8f3b64705250109a97e568838760
+# ngap branch: test-go-linter # eb97583fcd59ab76f2e6ae9adfbef299e399e7a7
 ./scripts/sync-sub-modules.py \
    --synchronize \
    --force \
    --gnbsim ngap-tester \
-   --nas ngap-tester \
-   --ngap ngap-tester
+   --nas test-go-linter \
+   --ngap test-go-linter
