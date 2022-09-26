e552e61f6cc41a6d62631c0a19c5990bd99d3612
Raphael Defosseux <raphael.defosseux@openairinterface.org>
2022-09-26 13:46:14 +0200

    fix(ci): fix typo
    Signed-off-by: Raphael Defosseux <raphael.defosseux@openairinterface.org>

--- scripts/sync-for-pull-request.sh
+++ scripts/sync-for-pull-request.sh
@@ -2,9 +2,12 @@
 # for each sub-module
 # If you want to re-trigger the CI jobs with the same branches
 # But new commits, just add the commit SHA-ONE as comment
+# gnbsim branch: 1367fe7d1b3b59e7ca58b49cd2ab6a416daa8c26
+# nas branch: N/A
+# ngap branch: N/A
 ./scripts/sync-sub-modules.py \
    --synchronize \
    --force \
-   --gnbsim dockerfile-optimization \ #1367fe7d1b3b59e7ca58b49cd2ab6a416daa8c26
+   --gnbsim dockerfile-optimization \
    --nas ngap-tester \
    --ngap ngap-tester
