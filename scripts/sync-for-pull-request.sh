968f3d2c98ac25f7bbe2de558c0b3476b24315f4
Raphael Defosseux <raphael.defosseux@openairinterface.org>
2022-09-26 13:57:37 +0200

    retrigger(ci)
    Signed-off-by: Raphael Defosseux <raphael.defosseux@openairinterface.org>

--- scripts/sync-for-pull-request.sh
+++ scripts/sync-for-pull-request.sh
@@ -2,7 +2,7 @@
 # for each sub-module
 # If you want to re-trigger the CI jobs with the same branches
 # But new commits, just add the commit SHA-ONE as comment
-# gnbsim branch: 1367fe7d1b3b59e7ca58b49cd2ab6a416daa8c26
+# gnbsim branch: 5fbe198c2249a18566bd7bdd0a5e0fa5ee944fb7
 # nas branch: N/A
 # ngap branch: N/A
 ./scripts/sync-sub-modules.py \
