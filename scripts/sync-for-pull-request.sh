8d4929c302f85f9942969bcc4f908801a82207f7
Raphael Defosseux <38105843+rdefosse@users.noreply.github.com>
2022-09-26 14:23:01 +0200

    chore(ci): testing the PR workflow (#1)
    * chore(ci): testing the PR workflow
    
    Signed-off-by: Raphael Defosseux <raphael.defosseux@openairinterface.org>

--- scripts/sync-for-pull-request.sh
+++ scripts/sync-for-pull-request.sh
@@ -1,8 +1,13 @@
 # Specify the branches or commits to be used by the CI workflows
 # for each sub-module
+# If you want to re-trigger the CI jobs with the same branches
+# But new commits, just add the commit SHA-ONE as comment
+# gnbsim branch: 5fbe198c2249a18566bd7bdd0a5e0fa5ee944fb7
+# nas branch: N/A
+# ngap branch: N/A
 ./scripts/sync-sub-modules.py \
    --synchronize \
    --force \
-   --gnbsim ngap-tester \
+   --gnbsim dockerfile-optimization \
    --nas ngap-tester \
    --ngap ngap-tester
