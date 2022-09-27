1c504d61b0c243c7e6bf77dd6c99f50d9721d77d
Raphael Defosseux <raphael.defosseux@openairinterface.org>
2022-09-27 17:14:23 +0200

    fix(ci): one last fix on gnbsim
    Signed-off-by: Raphael Defosseux <raphael.defosseux@openairinterface.org>

--- scripts/sync-for-pull-request.sh
+++ scripts/sync-for-pull-request.sh
@@ -2,12 +2,12 @@
 # for each sub-module
 # If you want to re-trigger the CI jobs with the same branches
 # But new commits, just add the commit SHA-ONE as comment
-# gnbsim branch: test-go-linter # b8f6a47ddbb5ea9eb89018edb02a9a1b50221860
+# gnbsim branch: ci-adding-golangci-lint # b0701f3bef06707a4670a23b484aea6347d22160
 # nas branch: test-go-linter # 7cfeb68a8a2d8f3b64705250109a97e568838760
 # ngap branch: test-go-linter # eb97583fcd59ab76f2e6ae9adfbef299e399e7a7
 ./scripts/sync-sub-modules.py \
    --synchronize \
    --force \
-   --gnbsim test-go-linter \
+   --gnbsim ci-adding-golangci-lint \
    --nas test-go-linter \
    --ngap test-go-linter
