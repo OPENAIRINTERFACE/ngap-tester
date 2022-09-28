96ced4f15964db60afd820acb19eb5274a7ed1f2
Raphael Defosseux <38105843+rdefosse@users.noreply.github.com>
2022-09-28 11:50:21 +0200

    chore(ci): adding golang linter (#2)
    * chore(ci): adding golang linter on all sub-modules
    
    Signed-off-by: Raphael Defosseux <raphael.defosseux@openairinterface.org>

--- scripts/sync-for-pull-request.sh
+++ scripts/sync-for-pull-request.sh
@@ -2,12 +2,12 @@
 # for each sub-module
 # If you want to re-trigger the CI jobs with the same branches
 # But new commits, just add the commit SHA-ONE as comment
-# gnbsim branch: 5fbe198c2249a18566bd7bdd0a5e0fa5ee944fb7
-# nas branch: N/A
-# ngap branch: N/A
+# gnbsim branch: ci-adding-golangci-lint # d733a6def588def03422d3809b427cff0bd75dda
+# nas branch: test-go-linter # 7cfeb68a8a2d8f3b64705250109a97e568838760
+# ngap branch: test-go-linter # eb97583fcd59ab76f2e6ae9adfbef299e399e7a7
 ./scripts/sync-sub-modules.py \
    --synchronize \
    --force \
-   --gnbsim dockerfile-optimization \
-   --nas ngap-tester \
-   --ngap ngap-tester
+   --gnbsim ci-adding-golangci-lint \
+   --nas test-go-linter \
+   --ngap test-go-linter
