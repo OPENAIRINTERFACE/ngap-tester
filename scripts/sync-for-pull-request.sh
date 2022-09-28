34bafbcd6730b4cc19318aa2c473f6fb278a0a86
Raphael Defosseux <raphael.defosseux@openairinterface.org>
2022-09-28 11:39:24 +0200

    chore(ci): retrigger
    Signed-off-by: Raphael Defosseux <raphael.defosseux@openairinterface.org>

--- scripts/sync-for-pull-request.sh
+++ scripts/sync-for-pull-request.sh
@@ -2,7 +2,7 @@
 # for each sub-module
 # If you want to re-trigger the CI jobs with the same branches
 # But new commits, just add the commit SHA-ONE as comment
-# gnbsim branch: ci-adding-golangci-lint # b0701f3bef06707a4670a23b484aea6347d22160
+# gnbsim branch: ci-adding-golangci-lint # d733a6def588def03422d3809b427cff0bd75dda
 # nas branch: test-go-linter # 7cfeb68a8a2d8f3b64705250109a97e568838760
 # ngap branch: test-go-linter # eb97583fcd59ab76f2e6ae9adfbef299e399e7a7
 ./scripts/sync-sub-modules.py \
