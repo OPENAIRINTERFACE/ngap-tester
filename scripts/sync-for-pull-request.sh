99d9c2b0b6f88fdc2a8fe81fdeae8f71d15cd393
Raphael Defosseux <raphael.defosseux@openairinterface.org>
2022-10-21 13:29:51 +0200

    chore(ci): trigger GHA
    Signed-off-by: Raphael Defosseux <raphael.defosseux@openairinterface.org>

--- scripts/sync-for-pull-request.sh
+++ scripts/sync-for-pull-request.sh
@@ -2,7 +2,7 @@
 # for each sub-module
 # If you want to re-trigger the CI jobs with the same branches
 # But new commits, just add the commit SHA-ONE as comment
-# gnbsim branch: upstream-2022-10-20-sync / e0eb9a9a05efdd40523f068dc8ae5aae453669b0
+# gnbsim branch: upstream-2022-10-20-sync / fb864d5141f0ceab2513c2071c92de2acf8f14c4
 # nas branch: N/A
 # ngap branch: N/A
 ./scripts/sync-sub-modules.py \
