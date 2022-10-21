dee2a1e2684556f4072f5b018c2b69e784371606
Raphael Defosseux <38105843+rdefosse@users.noreply.github.com>
2022-10-21 13:34:53 +0200

    chore(ci): upstream-2022-10-20-sync (#5)
    Signed-off-by: Raphael Defosseux <raphael.defosseux@openairinterface.org>

--- scripts/sync-for-pull-request.sh
+++ scripts/sync-for-pull-request.sh
@@ -2,12 +2,12 @@
 # for each sub-module
 # If you want to re-trigger the CI jobs with the same branches
 # But new commits, just add the commit SHA-ONE as comment
-# gnbsim branch: N/A
+# gnbsim branch: upstream-2022-10-20-sync / fb864d5141f0ceab2513c2071c92de2acf8f14c4
 # nas branch: N/A
 # ngap branch: N/A
 ./scripts/sync-sub-modules.py \
    --synchronize \
    --force \
-   --gnbsim golint-oct-fixes \
+   --gnbsim upstream-2022-10-20-sync \
    --nas ngap-tester \
    --ngap ngap-tester
