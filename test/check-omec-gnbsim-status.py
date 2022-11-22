d39eecd21b390aa6bace8505bc2fef20b10541c7
Raphael Defosseux <raphael.defosseux@openairinterface.org>
2022-11-22 15:20:23 +0100

    test(oai-cn5g): adding a 2nd gnbsim instance
    Signed-off-by: Raphael Defosseux <raphael.defosseux@openairinterface.org>

--- test/check-omec-gnbsim-status.py
+++ test/check-omec-gnbsim-status.py
@@ -24,7 +24,8 @@ logging.basicConfig(
     format="[%(asctime)s] %(name)s:%(levelname)s: %(message)s"
 )
 
-NB_PROFILES = 3
+NB_PROFILES = 4
+NB_PROFILES_2 = 1
 
 def main() -> None:
     logging.debug('\033[0;32m OMEC gnbsim RAN emulator started, checking if all profiles finished... takes few secs\033[0m....')
@@ -35,26 +36,35 @@ def main() -> None:
     notSilentForFirstTime = False
     status = 0
     for x in range(100):
-        cmd = f'docker logs omec-gnbsim 2>&1 | grep --colour=never "Profile " || true'
+        cmd = f'docker logs omec-gnbsim-1 2>&1 | grep --colour=never "Profile " | grep -v "Waiting for UEs to finish processing" || true'
         res = run_cmd(cmd, notSilentForFirstTime)
+        cmd2 = f'docker logs omec-gnbsim-2 2>&1 | grep --colour=never "Profile " | grep -v "Waiting for UEs to finish processing" || true'
+        res2 = run_cmd(cmd2, notSilentForFirstTime)
         notSilentForFirstTime = True
-        if res is None:
+        if res is None or res is None:
             exit(f'\033[0;31m Incorrect/Unsupported executing command "{cmd}"')
         cnt = res.count('Profile Status:')
+        cnt2 = res2.count('Profile Status:')
         passing = res.count('Profile Status: PASS')
-        if cnt == NB_PROFILES:
+        passing2 = res2.count('Profile Status: PASS')
+        if cnt == NB_PROFILES and cnt2 == NB_PROFILES_2:
             logging.debug('\033[0;32m All profiles finished\033[0m....')
-            if passing == NB_PROFILES:
+            if passing == NB_PROFILES and passing2 == NB_PROFILES_2:
                 logging.debug('\033[0;32m All profiles passed\033[0m....')
             else:
                 logging.error('\033[0;32m Some profiles failed\033[0m....')
                 status = -1
             print(res)
+            print(res2)
             break
         time.sleep(10)
-    if cnt != NB_PROFILES:
+    cmd = 'docker ps -a'
+    res = run_cmd(cmd, False)
+    print(res)
+    if cnt != NB_PROFILES or cnt2 != NB_PROFILES_2:
         logging.error('\033[0;31m Some profiles could not finish\033[0m....')
         print(res)
+        print(res2)
         sys.exit(-1)
     sys.exit(status)
 
